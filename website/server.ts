42963dcb874361c8bf712409abba661a6a96bd8d
Roux Sheikh <roux@sudocode1.xyz>
2021-12-06 19:39:25 +0000

    static on website

--- website/server.ts
+++ website/server.ts
@@ -1,16 +1,23 @@
 import express from 'express';
+import fs from 'fs';
 let app = express();
 
+let index = fs.readFileSync(require('path').join(process.cwd(), 'index.html'), 'utf-8');
+let style = fs.readFileSync(require('path').join(process.cwd(), 'style.css'), 'utf-8');
+let font = fs.readFileSync(require('path').join(process.cwd(), 'Poppins-Light.ttf'), 'utf-8');
+
 app.get('/', (req, res) => {
-    res.sendFile(require('path').join(process.cwd(), 'index.html'))
+    res.send(index);
 });
 
 app.get('/style.css', (req, res) => {
-    res.sendFile(require('path').join(process.cwd(), 'style.css'))
+    res.setHeader('Content-Type', 'text/css')
+    res.send(style);
 });
 
 app.get('/Poppins-Light.ttf', (req, res) => {
-    res.sendFile(require('path').join(process.cwd(), 'Poppins-Light.ttf'))
+    res.setHeader('Content-Type', 'font/ttf')
+    res.send(font);
 });
 
 app.listen(2020);
